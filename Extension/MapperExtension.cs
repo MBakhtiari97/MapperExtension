4d7ef0166a8be4a3fe8dc766be3411dc4b348b50
Mohammad Bakhtiari <mohammadba86@yahoo.com>
2025-06-02 22:58:19 +0330

    Add project files.

---
+++ Extension/MapperExtension.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+
+namespace MapperExtension.Extension;
+
+public static class MapperExtension
+{
+    public static TTarget Map<TSource, TTarget>(this TSource source) where TTarget : new()
+    {
+        if (source == null)
+            throw new ArgumentNullException(nameof(source));
+        var target = new TTarget();
+        var sourceProperties = typeof(TSource).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+        var targetProperties = typeof(TTarget).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+        foreach (var sourceProperty in sourceProperties)
+        {
+            var targetProperty = targetProperties.FirstOrDefault(p => 
+                p.Name == sourceProperty.Name && 
+                p.PropertyType == sourceProperty.PropertyType);
+
+            if (targetProperty != null && targetProperty.CanWrite)
+                targetProperty.SetValue(target, sourceProperty.GetValue(source));
+        }
+        return target;
+    }
+    public static IEnumerable<TTarget> MapCollection<TSource, TTarget>(this IEnumerable<TSource> source) where TTarget : new()
+    {
+        if (source == null) 
+            throw new ArgumentNullException(nameof(source));
+        return source.Select(item => item.Map<TSource, TTarget>());
+    }
+}
